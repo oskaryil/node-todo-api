131b46501bf0d779dc19fd7862237c4dfbd36cd5
antonmyr <15anmy@stockholmscience.se>
2017-05-06 11:57:28 +0200

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 let { mongoose } = require("./db/mongoose");
 let { Todo } = require("./models/todo");
@@ -35,6 +36,34 @@ app.get("/todos", (req, res) => {
   );
 });
 
+// GET /todos/1234324
+app.get("/todos/:id", (req, res) => {
+  let id = req.params.id;
+  // res.send(req.params);
+
+  // Valid id using isValid
+  if (!ObjectID.isValid(id)) {
+    // 404 - send back empty send
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+  // findById
+  //success
+  // if todo - send it back
+  // if no todo - send back 404 with empty body
+  //Error
+  //400 - and send empty body back
+});
 app.listen(3000, () => {
   console.log("Started on port 3000");
 });
