34df1e8c8393bf7501eec413ba04cc9ab2b5d899
antonmyr <15anmy@stockholmscience.se>
2017-05-08 10:07:04 +0200

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -65,6 +65,34 @@ app.get("/todos/:id", (req, res) => {
   //Error
   //400 - and send empty body back
 });
+
+app.delete("/todos/:id", (req, res) => {
+  // get the id
+  let id = req.params.id;
+
+  //validate the id -> not valid? return a 404
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // remove todo by id
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ deleted: todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+  //success
+  //if no doc send 404
+  //if doc send doc back with 200
+  //Error
+  //404 with empty body
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
