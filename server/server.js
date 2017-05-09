c6d450087c0e6d20b32594a34fc33286e3424b53
antonmyr <15anmy@stockholmscience.se>
2017-05-09 10:12:01 +0200

    Added tests for the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -81,7 +81,7 @@ app.delete("/todos/:id", (req, res) => {
       if (!todo) {
         return res.status(404).send();
       }
-      res.send({ deleted: todo });
+      res.send({ todo });
     })
     .catch(e => {
       res.status(400).send();
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -102,3 +102,43 @@ describe("GET /todos/:id", () => {
     request(app).get("/todos/123").expect(404).end(done);
   });
 });
+
+describe("DELETE /todos/:id", () => {
+  it("should remove a todo", done => {
+    let hexID = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexID)
+          .then(todo => {
+            expect(todo).toNotExist();
+            done();
+          })
+          .catch(e => {
+            done(e);
+          });
+        // query database using findById toNotExist
+        // expect(null).toNotExist();
+      });
+  });
+
+  it("should return 404 if todo not found", done => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return 404 if ObjectID is invalid", done => {
+    request(app).delete("/todos/123").expect(404).end(done);
+  });
+});
