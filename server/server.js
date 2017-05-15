fb72ce79b1dae59be55e483bfaca3641531bff2a
antonmyr <15anmy@stockholmscience.se>
2017-05-15 19:48:36 +0200

    Add DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -146,8 +146,6 @@ app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user);
 });
 
-//POST /users/login {email, password}
-
 app.post("/users/login", (req, res) => {
   let body = _.pick(req.body, ["email", "password"]);
 
@@ -160,10 +158,17 @@ app.post("/users/login", (req, res) => {
     .catch(e => {
       res.status(400).send(e);
     });
+});
 
-  // bcrypt.compare(body.password, hashedPassword, (err, res) => {
-  //   console.log(res);
-  // });
+app.delete("/users/me/token", authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(
+    () => {
+      res.status(200).send();
+    },
+    () => {
+      res.status(400).send();
+    }
+  );
 });
 
 app.listen(port, () => {
