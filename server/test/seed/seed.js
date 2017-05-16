dacdbaa066563668195125f325fc21c72e5c63da
antonmyr <15anmy@stockholmscience.se>
2017-05-16 10:40:25 +0200

    Update config to use json file

--- server/test/seed/seed.js
+++ server/test/seed/seed.js
@@ -16,7 +16,9 @@ const users = [
     tokens: [
       {
         access: "auth",
-        token: jwt.sign({ _id: userOneId, access: "auth" }, "abc123").toString()
+        token: jwt
+          .sign({ _id: userOneId, access: "auth" }, process.env.JWT_SECRET)
+          .toString()
       }
     ]
   },
@@ -27,7 +29,9 @@ const users = [
     tokens: [
       {
         access: "auth",
-        token: jwt.sign({ _id: userTwoId, access: "auth" }, "abc123").toString()
+        token: jwt
+          .sign({ _id: userTwoId, access: "auth" }, process.env.JWT_SECRET)
+          .toString()
       }
     ]
   }
